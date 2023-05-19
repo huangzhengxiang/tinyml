61568b70883656f43c700ee73c26f1428d07b753
Mike020728 <973924350@qq.com>
2023-05-19 12:55:54 +0800

    coco128 transfer learngin

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml").load('yolov8n.pt')  # build a new model from scratch
-# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
+# model = YOLO('runs/detect/VOC/weights/best.pt')
+model = YOLO('yolov8n.yaml')  # 'runs/detect/train13/weights/best.pt'
+# print(model.model.load_state_dict(torch.load('runs/detect/train13/weights/best.pt')['model']))
 # Use the model
 model.train(data="coco128.yaml", epochs=100)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
+metrics = model.val(data='coco128.yaml')  # evaluate model performance on the validation set
 results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-success = model.export(format="onnx")  # export the model to ONNX format
+# success = model.export(format="onnx")  # export the model to ONNX format
